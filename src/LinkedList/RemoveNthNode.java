690f48495b1f6c792b69cf82eca110ee9746d5d4
Ifeoluwapo123 <65716233+Ifeoluwapo123@users.noreply.github.com>
2021-09-29 21:27:26 +0100

    Update RemoveNthNode.java

--- src/LinkedList/RemoveNthNode.java
+++ src/LinkedList/RemoveNthNode.java
@@ -14,41 +14,30 @@
  * LEETCODE SOLUTION
  */
 class Solution {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
+   public ListNode removeNthFromEnd(ListNode head, int n) {
         var first = head;
         var second = head;
         
-        ListNode newNode = new ListNode(head.val);
-        
         for(int index = 0; index < n - 1; index++) second = second.next; 
-        
-        int counter = 1;
         
         while(second.next != null){
             
             second = second.next;
-            var current = newNode;
+            var current = first.next;
             
             if(second.next == null){
-//                 first.next = second;
-//                 for(int index = 0; index < counter - 1; index++) current = current.next;
-//                 System.out.println(current.val);
-                
-//                 // newNode.val = second.val;
-                
-                // newNode = new ListNode(second.val, newNode);
+                first.next = second;
+                current.next = null;
+                current = null;
                 break;
             }
             
             first = first.next;
             
-            newNode = new ListNode(first.val, newNode);
-            counter++;
- 
         }
         
+        return head;
         
-        return newNode;
     }
     
 }
